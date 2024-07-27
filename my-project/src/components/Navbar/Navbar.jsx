dab7f7fc9900907244429c3a94bd6a2224ac9d56
Vetanshu <vetanshu.rajoria@gmail.com>
2024-07-28 01:16:31 +0530

    another commit

--- my-project/src/components/Navbar/Navbar.jsx
+++ my-project/src/components/Navbar/Navbar.jsx
@@ -25,25 +25,25 @@ const Header = () => {
         duration: 0.8,
       }}
     >
-      <nav className="bg-slate-950 px-4 lg:px-6 py-2.5">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+      <nav className="bg-slate-950 px-4 sm:px-6 py-2.5">
+        <div className="flex  justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
               src={Image}
-              className="mr-3 h-8 sm:h-10 md:h-12"
+              className="mr-3 h-4 sm:h-8 md:h-10 lg:h-12"
               alt="Logo"
             />
           </Link>
           <div className="flex items-center lg:order-2">
-            <Link
+            <Link 
               to="#"
-              className="text-white hover:bg-indigo-600 focus:ring-1 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none hover:scale-105 transition-all 300"
+              className="text-white hover:bg-indigo-600 focus:ring-1 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 sm:px-5 py-2 mr-2 focus:outline-none hover:scale-105 transition-all duration-300"
             >
               Login
             </Link>
             <Link
               to="#"
-              className="text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl font-semibold focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-gray-900 text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 bg-gradient-to-r from-fuchsia-900 to-violet-900 hover:bg-gradient-to-l hover:scale-105 200"
+              className="text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl font-semibold focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-gray-900 text-xs px-4 sm:px-5 py-2 mr-2 bg-gradient-to-r from-fuchsia-900 to-violet-900 hover:bg-gradient-to-l hover:scale-105"
             >
               Sign up
             </Link>
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       },
 
       screens:{
-        "phone":"200px"
+        "sm":"320px"
       },
 
       lineHeight:{
